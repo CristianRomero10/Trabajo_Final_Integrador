75bf7f9723415c21176365990cd6836617998f78
cristianms1009 <cristian_romero_21@hotmail.com>
2022-09-26 02:25:42 -0300

    agregamos proyectos y le dimos estilos

--- Integrador/src/app/app.module.ts
+++ Integrador/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { BannerComponent } from './componentes/banner/banner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './componentes/hys/hys.component';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +23,19 @@ import { EducacionComponent } from './componentes/educacion/educacion.component'
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
-  providers: [],
+
+  providers:[],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+  export class AppModule { } 
+
+
